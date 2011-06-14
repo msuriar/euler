bd76f117a41992d96208163597283be187bece31
Murali Suriar <code@suriar.com>
2011-06-14 22:28:17 +0100

    Double counting. Moron.

--- 01/solution.py
+++ 01/solution.py
@@ -2,5 +2,5 @@
 lowerLimit = 0
 upperLimit = 1000
 
-total = sum(xrange(lowerLimit,upperLimit,3)) + sum(xrange(lowerLimit,upperLimit,5))
+total = sum(xrange(lowerLimit,upperLimit,3)) + sum(xrange(lowerLimit,upperLimit,5)) - sum(xrange(lowerLimit,upperLimit,15))
 print total

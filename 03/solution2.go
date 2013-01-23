8acd618a428b389cb1b1cba230b64c77209fe688
Murali Suriar <code@suriar.com>
2013-01-23 00:22:01 +0000

    Approach 2. Time for bed.

---
+++ 03/solution2.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+  "fmt"
+)
+
+const (
+  int64 target = 600851475143
+)
+func main() {
+  t := target
+  f := int64(2)
+  tried := []int64{}
+  tried = append(tried, f)
+  for {
+    for t % f == 0 {
+
+    }
+  }
+}
